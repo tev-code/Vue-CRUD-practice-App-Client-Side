dd9698f04d4236bacabacc37f128ee9339394e68
Tevin <tevin@trade245.com>
2024-04-17 22:10:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<!-- @format -->
+
+<template>
+  <v-app>
+    <v-app-bar app dark>
+      <div class="d-flex align-center mr-2">tevCode</div>
+
+      <v-btn to="/tutorials" text>Tutorials</v-btn>
+
+      <v-btn to="/add">Add</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: "app",
+  };
+</script>
